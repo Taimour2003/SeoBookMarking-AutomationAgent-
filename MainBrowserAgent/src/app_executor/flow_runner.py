5f35351c0cbe52be5a44262300be76b77d8274e1
Taimour Tariq <taimourtariq2003@gmail.com>
2026-09-09 15:11:24 +0500

    bookmarkingurls_fetching_resolved2

--- MainBrowserAgent/src/app_executor/flow_runner.py
+++ MainBrowserAgent/src/app_executor/flow_runner.py
@@ -26,7 +26,7 @@ from panel.safely_install_panel import safely_install_panel
 from playwright.async_api import async_playwright
 from signup_form.signup_form_filling import fill_signup_form
 from submission.assistant import SubmissionAssistant
-from fallback_urls import FALLBACK_BOOKMARKING_URLS
+from .fallback_urls import FALLBACK_BOOKMARKING_URLS
 
 
 class FlowRunner:
